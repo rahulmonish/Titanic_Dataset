da54dca21298172b1dcccd8a13d86a1a39016245
U65988 <rahul.monish@ust-global.com>
2020-02-25 18:15:04 +0530

    Added Family count, changed age conversion

--- titanic 2.py
+++ titanic 2.py
@@ -23,17 +23,27 @@ from scipy.stats import norm
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import log_loss
 import seaborn as sns
-#import xgboost as xgb
+import xgboost as xgb
 
 
 
 
 
 
-
+import keras
 from keras.models import Sequential
-from keras.layers import Dense
+from keras.layers import Dense, Dropout
 from keras.wrappers.scikit_learn import KerasClassifier
+
+
+def name_processing(df):
+    list1= list(df['Name'])
+    for i in list1:
+        name_list= i.split(',')
+        last_name= name_list[0]
+
+    return df
+
 
 def calculate_average(values):
     value_dict= {}
@@ -70,13 +80,17 @@ def check_column(df, df2):
     return df,df2
 
 def change_age(df):
-    for i in range(0,len(df)):
-        df['Age'].at[i]= int(df['Age'].at[i]/5)
+    df.loc[df['Age']>16, 'Age']=1
+    df.loc[df['Age']<16, 'Age']=0
+    #for i in range(0,len(df)):
+    #    df['Age'].at[i]= int(df['Age'].at[i]/5)
+        #if df['Age'].at[i]>16:
+            
     
     return df
 
 def convert_Dataframe(df):
-    df= df.drop([ 'Fare', 'Name','Ticket', 'Cabin'], axis=1)
+    df= df.drop([ 'Name', 'Fare', 'Ticket', 'Cabin'], axis=1)
     
     df['Sex']= label_encoder.fit_transform(df['Sex'])
     
@@ -91,7 +105,10 @@ def convert_Dataframe(df):
     df= df.drop(['Embarked'], axis=1)
     return df
 
-
+def remove_parch_sibsp(df):
+    df['Family Count']= df['Parch']+df['SibSp']
+    df= df.drop(['SibSp', 'Parch'], axis=1)
+    return df
 
 def preprocess(df):
     rich_mean= np.mean(df[df['Pclass']==1]['Age'])
@@ -114,14 +131,15 @@ def preprocess(df):
 
 label_encoder = preprocessing.LabelEncoder() 
 df= pd.read_csv('train.csv')
-sns.pairplot(df,hue= 'Survived', diag_kind='hist', height=1)
+#sns.pairplot(df,hue= 'Survived', diag_kind='hist', height=1)
 df.isnull().sum()
 correlation_plot= df.corr(method= 'pearson')
 print("before correlation: " ,df['Age'].corr(df['Survived']))
-sns.distplot(df['Age'].dropna())
-sns.countplot(x='Survived', hue= 'Sex', data= df)
+#sns.distplot(df['Age'].dropna())
+#sns.countplot(x='Survived', hue= 'Sex', data= df)
 
-
+#df= name_processing(df)
+df= remove_parch_sibsp(df)
 df= preprocess(df)
 df= change_age(df)
 df= convert_Dataframe(df)
@@ -152,19 +170,20 @@ count=1
 #     plt.show()
 # =============================================================================
 
-#model= xg_reg = xgb.XGBRegressor(objective ='reg:logistic', colsample_bytree = 0.3, 
-#learning_rate = 0.1, alpha = 10, n_estimators = 10)
+model= xg_reg = xgb.XGBRegressor(objective ='binary:logistic', max_depth= 3, learning_rate = 0.1)
 #model= LogisticRegression()
-model = svm.SVC()
+#model = svm.SVC()
 #model= DecisionTreeClassifier()
 #model= RandomForestClassifier()
 # =============================================================================
+# sgd = keras.optimizers.SGD(lr=0.5, clipvalue=0.5)
+# es = keras.callbacks.EarlyStopping(monitor='val_loss', mode='min', verbose=1)
 # model = Sequential()
-# model.add(Dense(100, input_dim=23, activation='relu'))
-# model.add(Dense(50,  activation='relu'))
-# model.add(Dense(10,  activation='sigmoid'))
+# model.add(Dense(3, input_dim=8, activation='relu'))
+# #model.add(Dense(50,  activation='tanh'))
+# #model.add(Dropout(10))
 # model.add(Dense(1,  activation='sigmoid'))
-# model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+# model.compile(loss='binary_crossentropy', optimizer=sgd, metrics=['accuracy'])
 # =============================================================================
 #model.fit(x,y)
 # =============================================================================
@@ -181,12 +200,15 @@ model = svm.SVC()
 test_set= pd.read_csv('test.csv')
 passenger= test_set['PassengerId']
 
+test_set= remove_parch_sibsp(test_set)
 test_set= preprocess(test_set)
 test_set= change_age(test_set)
 test_set= convert_Dataframe(test_set)
 
 df, test_set= check_column(df, test_set)
-#df['Age']= calculate_average(list(df['Age']))
+df['Age']= calculate_average(list(df['Age']))
+scaler = preprocessing.StandardScaler()
+df = scaler.fit_transform(df)
 # =============================================================================
 # df = rename_columns(df)
 # test_set= rename_columns(test_set)
@@ -195,15 +217,16 @@ df, test_set= check_column(df, test_set)
 # columns= df.columns
 # columns
 # =============================================================================
-x=df[['Parch', 'PassengerId']]
+x=df
 model.fit(x,y)
-#model.fit(x,y, epochs=1000)
+#model.fit(x,y, epochs=1000,callbacks=[es])
 
 
 test_set= test_set.fillna(0)
-#test_set['Age']= calculate_average(list(test_set['Age']))
+test_set['Age']= calculate_average(list(test_set['Age']))
 
-x_predict= test_set[['Parch', 'PassengerId']]
+x_predict= test_set
+x_predict = scaler.fit_transform(x_predict)
 
 final= model.predict(x_predict)
 
@@ -217,11 +240,10 @@ convert_dict = {'Survived': int}
 for i in range(0,len(test_set)):
     if test_set['Survived'].at[i]< .5:
        test_set['Survived'].at[i]= int(0)
-       print(type(test_set['Survived'].at[i]))
     else:
         test_set['Survived'].at[i]=int(1)
 
 test_set = test_set.astype(convert_dict) 
 test_set.to_csv('attempt1.csv',  index=False)
-sns.countplot(test_set['Survived'])
+#sns.countplot(test_set['Survived'])
 test_set.isnull().sum()
